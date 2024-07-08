5c35ef1cb15a59f2b72b76fa018e7d33221b679c
batunpc <batuhnpc@gmail.com>
2024-07-08 02:05:10 -0400

    add missing header

--- server.js
+++ server.js
@@ -17,8 +17,23 @@ const corsOptions = {
   optionsSuccessStatus: 204,
 };
 app.use(cors(corsOptions));
-
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', corsOptions.origin);
+  res.header(
+    'Access-Control-Allow-Methods',
+    corsOptions.methods.join(','),
+  );
+  res.header(
+    'Access-Control-Allow-Headers',
+    corsOptions.allowedHeaders.join(','),
+  );
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+app.options('*', cors(corsOptions));
 
 // JSON Web Token Setup
 const ExtractJwt = passportJWT.ExtractJwt;
